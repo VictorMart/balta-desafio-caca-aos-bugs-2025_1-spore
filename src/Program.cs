35659d889c4df0469ecfb30c4fbd119d6f9b149f
Victor Martinelli <victmartinelli@gmail.com>
2025-10-26 14:32:02 -0300

    Finalizado

--- src/Program.cs
+++ src/Program.cs
@@ -1,4 +1,13 @@
+using BugStore.Data;
+using Microsoft.EntityFrameworkCore;
+
 var builder = WebApplication.CreateBuilder(args);
+
+var connetionString = builder.Configuration.GetConnectionString("DefaultConnection");
+builder.Services.AddDbContext<AppDbContext>(options =>
+    options.UseSqlite(connetionString));
+
+
 var app = builder.Build();
 
 app.MapGet("/", () => "Hello World!");
